4134c9acfd07b4760d508b3c5551e68228673954
sachingupta67 <sachingupta769@outlook.com>
2020-08-10 10:25:36 +0530

    changes

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,6 @@
-const {createStore, compose} = require('redux');
-const rootReducer = require('./reducers');
+import {createStore, compose} from 'redux';
+import rootReducer from './reducers';
+
 let composeEnhancers = compose;
 if (__DEV__) {
   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
